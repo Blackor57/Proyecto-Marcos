df78eeb886ced7c78443c42c6cadebae4e289eaa
Blackor57 <jtinipuclla2@gmail.com>
2024-10-08 10:33:11 -0500

    holi

---
+++ src/main/java/com/example/proyectomarcos/ProyectoMarcosApplication.java
@@ -0,0 +1,13 @@
+package com.example.proyectomarcos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoMarcosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoMarcosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/proyectomarcos/ProyectoMarcosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyectomarcos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoMarcosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
